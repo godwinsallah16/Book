a0cc0dbbf5e96a1347454f70f403a87207e8e14e
godwinsallah16 <godwinsallah16@gmail.com>
2025-07-12 01:42:28 +0000

    Fix package version compatibility - downgrade EF Core to 8.0.11 to match .NET 8 framework

--- BookStore.API/obj/Debug/net8.0/BookStore.API.AssemblyInfo.cs
+++ BookStore.API/obj/Debug/net8.0/BookStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca6a70ffe21851e934899f4cb80e88c0a3ecb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f4b33b5ec694f88267c11cf09d99689d7acb36")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
